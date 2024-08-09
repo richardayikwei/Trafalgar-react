c58ae3bba1916f2918d4e6bf3a9588c0eecc9c35
Richard Ayikwei <richardayikweibee@gmail.com>
2024-08-09 06:41:38 +0000

    Organising assests

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,13 +1,54 @@
-import element from "./images/element.png";
-import illustration from "./images/illustration.png";
+import element from "./bgimages/element.png";
+import element1 from "./bgimages/element1.png";
+import element2 from "./bgimages/element2.png";
+import vector from "./bgimages/vector.png";
+import vector1 from "./bgimages/vector1.png";
+import favicon from './favicon/favicon.png';
+import bag from './icons/bag.png';
 import searchIcon from "./icons/search.png";
+import bottleIcon from "./icons/bottle.png";
+import chartIcon from "./icons/chart.png";
+import infoIcon from "./icons/info.png";
+import info2Icon from "./icons/info2.png";
+import illustration from "./images/illustration.png";
+import micorscoping from "./images/micorscoping.png";
+import facial from "./images/facial.png";
+import illustration2 from "./images/illustration2.png";
+import illustration3 from "./images/illustration3.png";
+import injection from "./images/injection.png";
+import headshot from './profilepics/headshot.png'
 
 export const assets = {
-  images: {
+  bgimages: {
     element,
-    illustration,
+    element1,
+    element2,
+    vector,
+    vector1,
   },
+
+  favicon: {
+    favicon
+  },
+
   icons: {
     searchIcon,
+    bag,
+    bottleIcon,
+    chartIcon,
+    infoIcon,
+    info2Icon,
   },
+  images: {
+    illustration,
+    micorscoping,
+    facial,
+    illustration2,
+    illustration3,
+    injection,
+  },
+  profilepics: {
+    headshot
+  }
+
 };
