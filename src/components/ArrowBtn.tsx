93d3865b3611bca0c65d4061a77ea3206daf6264
Richard Ayikwei <richardayikweibee@gmail.com>
2024-08-10 15:52:24 +0000

    hover effect for article buttons

--- src/components/ArrowBtn.tsx
+++ src/components/ArrowBtn.tsx
@@ -3,7 +3,7 @@
 const ArrowBtn = () => {
   return (
     <div className="mb-6">
-      <a href="#" className="text-blue-400 p-3">
+      <a href="#" className="text-blue-400 hover:text-blue-600 p-3">
         Read more <span className="text-2xl ">&#8594;</span>
       </a>
     </div>
