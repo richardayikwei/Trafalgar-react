6f0152cc9585bbe3d8005e012e94aa6e2b1412a8
Richard Ayikwei <richardayikweibee@gmail.com>
2024-08-12 15:23:21 +0000

    service heading

--- src/App.tsx
+++ src/App.tsx
@@ -14,17 +14,19 @@ function App() {
   return (
     <>
       <Navbar />
-      {/* <section>
-        <div className="hidden w-[131.58px] h-[115px] mt-[63px] md:flex ">
-          <img src={assets.bgimages.element} alt="element" />
-        </div>
-        <div className="flex flex-col-reverse md:flex-row md:mt-0 mt-[200px]">
-          <div className="flex flex-col md:w-[445px] space-y-9 w-[445px] h-[338px] rounded-[55px] md:ml-[190px] ">
+
+      <div className="hidden w-[131.58px] h-[115px] mt-[63px] md:flex ">
+        <img src={assets.bgimages.element} alt="element" />
+      </div>
+
+      <section className="container mx-auto">
+        <div className="flex flex-col-reverse md:flex-row md:mt-0 mt-[200px] justify-between">
+          <div className="flex flex-col md:w-[445px] space-y-9 w-[445px] h-[338px] rounded-[55px]">
             <h1 className="text-[48px] h-[112px] md:text-left text-center w-[427px] font-bold leading-[56px]">
               Virtual healthcare for you
             </h1>
 
-            <p className="text-[#7D7987] md:text-left text-center text-[21px] leading-[32px] font-light ">
+            <p className="text-[#7D7987] md:text-left text-center text-[21px] leading-[32px] font-[300]">
               Trafalgar provides progressive, and affordable healthcare,
               accessible on mobile and online for everyone
             </p>
@@ -35,29 +37,29 @@ function App() {
               </button>
             </div>
           </div>
-          <div className="md:w-[693px] md:h-[598px] md:ml-[109px] -mt-16">
+          <div className="md:w-[693px] md:h-[598px] -mt-16">
             <img src={assets.images.illustration} alt="illustration" />
           </div>
         </div>
-      </section> */}
-      {/* <div className="flex flex-col items-center space-y-5 h-[177px] ml-[323px] w-[952px] mt-[200px] ">
+      </section>
+      <div className="flex flex-col items-center space-y-5 md:h-[177px] md:w-[952px] mt-[200px] container mx-auto ">
         <div className="w-[212px] font-bold leading-[56px] text-[36px]">
           <h1>Our services</h1>
         </div>
 
         <div className="flex ">
-          <p className="border-b-2 w-10 border-black"></p>
+          <p className="border-b-2 w-[56px] border-black"></p>
         </div>
 
-        <div className="w-[952px] h-[60px] font-[300] text-[#7D7987] text-center">
+        <div className="md:w-[952px] md:h-[60px] font-[300] text-[#7D7987] text-center leading-[30px]">
           <p>
-            We provide to you the best choices for you. Adjust it to your health
-            needs and make sure undergo treatment with our highly qualified
-            doctors you can consult with us which type of service is suitable
-            for your health
+            We provide to you the best choiches for you. Adjust it to your
+            health needs and make sure your undergo treatment with our highly
+            qualified doctors you can consult with us which type of service is
+            suitable for your health
           </p>
         </div>
-      </div> */}
+      </div>
       {/* <section>
         <div className=" flex flex-col mx-auto mt-16 ml-[240px] w-[1120px] h-[745px] ">
           <div className="grid grid-cols-3 gap-x-[34px] gap-y-[37px]">
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav className="container mx-auto">
       <div className="flex flex-col md:flex-row md:justify-between">
         
-        <div className="flex-col md:flex-row w-[160px] h-[41px] mt-[56px] ">
+        <div className="flex-col md:flex-row w-[160px] h-[41px] mt-[56px] ml-[40px] ">
           <h1 className="text-[#233348] font-bold text-[24px]">
             <span className="rounded-full bg-[#458FF6] text-[26px] py-3 px-6 mr-3 font-bold  text-white">
               T
@@ -16,7 +16,7 @@ const Navbar = () => {
           </h1>
         </div>
        
-        <div className="flex flex-col md:flex-row md:space-x-[40px] ml-[40px] l w-[545px] h-[23px] mt-[70px] text-[18px] text-[#1F1534] ">
+        <div className="flex flex-col md:flex-row md:space-x-[40px] ml-[40px] w-[545px] h-[23px] mt-[70px] text-[18px] text-[#1F1534] ">
           <a
             href="#"
             className="font-bold hover:bg-[#1F1534] w-[51px] hover:text-white"
