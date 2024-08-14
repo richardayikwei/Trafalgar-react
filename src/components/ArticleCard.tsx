95d237a033886ee85ea8663e5d83f2c22f5a835f
Richard Ayikwei <richardayikweibee@gmail.com>
2024-08-14 17:29:11 +0000

    marging adjustment

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -13,13 +13,12 @@ const SectionFour = ({article, children}: ArticleCardProps) => {
         alt={article.alt}
         className="md:w-[350px] md:h-[240px]"
       />
-      <div className=" mt-[12px] space-y-[12px] flex flex-col md:mx-auto ml-2 md:ml-0">
+      <div className=" mt-[12px] space-y-[12px] j flex flex-col md:mx-auto ml-2">
 
         <h1 className="h-[64px] w-[266px] text-[21px] font-bold leading-[32px]">
           {article.title}
         </h1>
      
-      
         <p className="w-[266px] h-[84px] text-[16px] leading-[28px] font-[300] text-[#7D7987]">
           {article.text}
         </p>
