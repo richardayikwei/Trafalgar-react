5ac3b00d8925d50a13c00cdf72a09855ff70cf0a
Richard Ayikwei <richardayikweibee@gmail.com>
2024-08-16 11:05:37 +0000

    flex for button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,7 @@ interface ButtonProps {
 
 const Button = ({btntext, position} : ButtonProps) => {
   return (
-    <div className={`md:flex ${position} text-center mt-[70px]`}>
+    <div className={`flex ${position} text-center mt-[70px]`}>
       <a
         href="#"
         className="bg-white  border-[#458FF6] text-[#458FF6] text-[18px] border-[1.4px] rounded-[55px] border-solid leading-[60px] hover:bg-blue-700 min-h-[60px] min-w-[200px] hover:text-white py-[10px] px-[5px] md:py-0 md:px-0"
