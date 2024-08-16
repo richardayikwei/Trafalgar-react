bf7c2fff76f147c792693cd389c2644ca9d0907b
Richard Ayikwei <richardayikweibee@gmail.com>
2024-08-16 10:14:39 +0000

    minmum height for buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ const Button = ({btntext, position} : ButtonProps) => {
     <div className={`md:flex ${position} text-center mt-[70px]`}>
       <a
         href="#"
-        className="bg-white  border-[#458FF6] text-[#458FF6] text-[18px] border-[1.4px] rounded-[55px] border-solid leading-[60px] hover:bg-blue-700 h-[60px] w-[200px] hover:text-white py-[10px] px-[5px] md:py-0 md:px-0"
+        className="bg-white  border-[#458FF6] text-[#458FF6] text-[18px] border-[1.4px] rounded-[55px] border-solid leading-[60px] hover:bg-blue-700 min-h-[60px] w-[200px] hover:text-white py-[10px] px-[5px] md:py-0 md:px-0"
       >
         {btntext}
       </a>
