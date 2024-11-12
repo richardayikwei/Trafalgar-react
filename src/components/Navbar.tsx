c6a137f98b78a0494caaebc0439057c9c6e1fea2
Richard Ayikwei <richardayikweibee@gmail.com>
2024-11-12 09:56:05 +0000

    push to update web

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,7 @@
-
-
 const Navbar = () => {
-
-
   return (
     <nav className="container mx-auto">
       <div className="flex flex-col md:flex-row md:justify-between">
-        
         <div className="flex-col md:flex-row w-[160px] h-[41px] mt-[56px] ml-[40px] md:ml-0 ">
           <h1 className="text-[#233348] font-bold text-[24px]">
             <span className="rounded-full bg-[#458FF6] text-[26px] py-4 px-6 mr-3 font-bold  text-white">
@@ -15,7 +10,7 @@ const Navbar = () => {
             Trafalgar
           </h1>
         </div>
-       
+
         <nav className="flex flex-col md:flex-row md:space-x-[40px] ml-[40px] md:w-[545px] h-[23px] mt-[70px] text-[18px] text-[#1F1534] ">
           <a
             href="#"
@@ -29,7 +24,10 @@ const Navbar = () => {
           >
             Find a doctor
           </a>
-          <a href="#" className="opacity-50 w-[44px] md:hover:bg-[#1F1534] md:hover:text-white">
+          <a
+            href="#"
+            className="opacity-50 w-[44px] md:hover:bg-[#1F1534] md:hover:text-white"
+          >
             Apps
           </a>
           <a
@@ -38,13 +36,16 @@ const Navbar = () => {
           >
             Testimonials
           </a>
-          <a href="#" className="opacity-50 w-[76px] md:hover:bg-[#1F1534] md:hover:text-white">
+          <a
+            href="#"
+            className="opacity-50 w-[76px] md:hover:bg-[#1F1534] md:hover:text-white"
+          >
             About Us
           </a>
         </nav>
       </div>
     </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
